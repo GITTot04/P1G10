3275b6a9bb4ad9aee43b85f773d56651835fdc06
GITTot04 <Christiankirkebjergthorup@gmail.com>
2024-11-18 21:03:34 +0100

    Healthbar + restart button

--- P1G10/Assets/Scripts/AnswerButton.cs
+++ P1G10/Assets/Scripts/AnswerButton.cs
@@ -8,22 +8,36 @@ public class AnswerButton : MonoBehaviour
 {
     GameObject feedbackText;
     GameObject gameController;
+    GameObject healthBar;
     public string buttonValue;
     private void Start()
     {
         feedbackText = GameObject.Find("AnswerFeedbackText");
         gameController = GameObject.Find("GameMaster");
+        healthBar = GameObject.Find("Healthbar");
     }
     public void Answer()
     {
-        if (buttonValue == gameController.GetComponent<GameController>().currentAnswer)
+        if (healthBar.GetComponent<HealtbarScript>().currentHealth != 0)
         {
-            feedbackText.GetComponent<TextMeshProUGUI>().text = "Korrekt!";
+            if (buttonValue == gameController.GetComponent<GameController>().currentAnswer)
+            {
+                feedbackText.GetComponent<TextMeshProUGUI>().text = "Korrekt!";
+            }
+            else
+            {
+                healthBar.GetComponent<HealtbarScript>().currentHealth -= 1;
+                healthBar.GetComponent<HealtbarScript>().TookDamage();
+                if (healthBar.GetComponent<HealtbarScript>().currentHealth != 0)
+                {
+                    feedbackText.GetComponent<TextMeshProUGUI>().text = "Prøv igen!";
+                }
+                else
+                {
+                    feedbackText.GetComponent<TextMeshProUGUI>().text = "Øv!";
+                }
+            }
+            gameController.GetComponent<GameController>().LoadNewValues();
         }
-        else
-        {
-            feedbackText.GetComponent<TextMeshProUGUI>().text = "Prøv igen!";
-        }
-        gameController.GetComponent<GameController>().LoadNewValues();
     }
 }
---
+++ P1G10/Assets/Scripts/HealtbarScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealtbarScript : MonoBehaviour
+{
+    public Sprite emptyHealth;
+    public int currentHealth = 3;
+    GameObject restartButton;
+    private void Awake()
+    {
+        restartButton = GameObject.Find("RestartButton");
+        restartButton.SetActive(false);
+    }
+
+    public void TookDamage()
+    {
+        switch (currentHealth)
+        {
+            case 2:
+                transform.GetChild(2).GetComponent<Image>().sprite = emptyHealth;
+                break;
+            case 1:
+                transform.GetChild(1).GetComponent<Image>().sprite = emptyHealth;
+                break;
+            case 0:
+                transform.GetChild(0).GetComponent<Image>().sprite = emptyHealth;
+                restartButton.SetActive(true);
+                // add "You lost" message or something here
+                break;
+        }
+    }
+
+
+
+}
