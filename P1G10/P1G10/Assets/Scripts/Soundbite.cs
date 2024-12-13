7060fd7d87e7aa13ecf07b376adf72d4a68bafc1
ArlynAAU <UF77ZA@student.aau.dk>
2024-12-13 18:55:59 +0100

    Revert "Squashed commit of the following:"
    This reverts commit 35f4df5661c6b3ca28e379fdb9d0fbc9761d804c.

--- P1G10/P1G10/Assets/Scripts/Soundbite.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Soundbite : MonoBehaviour
-{
-    public string value;
-    public new AudioClip audio;
-}
